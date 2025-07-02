84fe4c221b7623b69d0374cac92891fd32365988
Cryspysam <ssmishra9918@gmail.com>
2025-07-02 18:49:48 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import "./App.css"
 import Title from "./Title.jsx";
 
-function Description() {
-  return <h3>I am the Description!</h3>;
-}
-
 function App(){
   return(
-   <div>
+   <div className="mainbox">
     <Title/>
-    <Description/>
      <Title/>
-    <Description/>
    </div>
   );
 }

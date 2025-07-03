55549221bb05aa0e09e271357b719aa14f3c49ad
Cryspysam <ssmishra9918@gmail.com>
2025-07-03 17:43:16 +0530

    Initial commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,13 @@
-import "./Product.css"
+import "./Product.css";
 
-function Product() {
-    return (
-        <div className="Product">
-            <h3>Product title</h3>
-            <h5>Product Description</h5>
-        </div>
-    )
+function Product({title,price=1}) {
+  //console.log(props.title);
+  return (
+    <div className="Product">
+      <h3>{title}</h3> 
+      <h5>Price: {price}</h5>
+    </div>
+  );
 }
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,15 +1,14 @@
 import Product from "./Product.jsx";
 
-function ProductTab(){
-    return(
-    
-     <>
-     <Product/>
-     <Product/>
-     <Product/>
-     <Product/>
-     </>
-    );
+function ProductTab() {
+  return (
+    <>
+      <Product title="phone" price={10000}/>
+      <Product title="laptop" price={20000}/>
+      <Product title="pen" price={120}/>
+      <Product title="Copy" />
+    </>
+  );
 }
 
 export default ProductTab;

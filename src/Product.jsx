75e74947446b2a285b8e6ff1099726917b1a96b1
Cryspysam <ssmishra9918@gmail.com>
2025-07-03 18:25:05 +0530

    Initial commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,11 +1,11 @@
 import "./Product.css";
 
-function Product({title,price=1}) {
-  //console.log(props.title);
+function Product({ title, price, features }) {
   return (
     <div className="Product">
       <h3>{title}</h3> 
       <h5>Price: {price}</h5>
+      <p>Features: {features ? Object.values(features).join(", ") : "No features"}</p>
     </div>
   );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,12 +1,14 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+    let options =["hi-tech", "durable", "fast"];
+   // let options2 ={a: "hi-tech", b:"durable", c:"fast"}
   return (
     <>
-      <Product title="phone" price={10000}/>
-      <Product title="laptop" price={20000}/>
-      <Product title="pen" price={120}/>
-      <Product title="Copy" />
+      <Product title="phone" price={10000} features={{a:"hi-tech"}}/>
+      {/* <Product title="laptop" price={20000} /> */}
+      {/* <Product title="pen" price={120}/> */}
+      {/* <Product title="Copy" price={200} /> */}
     </>
   );
 }

96c8ed28484afc043dea50c9ed8de0b849cb4318
Cryspysam <ssmishra9918@gmail.com>
2025-07-04 11:23:40 +0530

    Initial commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,8 +1,9 @@
 import "./Product.css";
 
 function Product({title,price, features}) {
+  let styles = {backgroundColor : price>30000 ? "pink" : ""};
   return (
-    <div className="Product">
+    <div className="Product"  style={styles}>
       <h3>{title}</h3> 
       <h5>Price: {price}</h5>
       {price> 30000 && <p>Discpunt of 5%</p>}

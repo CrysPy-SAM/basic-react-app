85db428136f71dc99632de95e13c7bbd04da1975
Cryspysam <ssmishra9918@gmail.com>
2025-07-04 14:29:16 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css"
 import Title from "./Title.jsx";
 import ProductTab from "./ProductTab.jsx";
+import MsgBox from "./MsgBox.jsx";
 
-function App(){
-  return <ProductTab />;
-  
+function App() {
+  return (
+    <>
+      <MsgBox userName="Satyam" textColor="red" />
+      <MsgBox userName="Omi" textColor="green" />
+      <MsgBox userName="ram" textColor="orange" />
+      <ProductTab />
+    </>
+  );
 }
+
 
 export default App;

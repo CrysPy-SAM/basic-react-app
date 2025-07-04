53dac6d151ab02329d8a7e4bceaa9d2bca517bc4
Cryspysam <ssmishra9918@gmail.com>
2025-07-04 16:26:55 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import "./App.css"
+import "./App.css";
 import ProductTab from "./ProductTab.jsx";
-
+import Button from "./button.jsx";
 
 function App() {
   return (
     <>
-         <h2>Blockbuster Deals | Shop Now</h2>
-      <ProductTab />
+      <Button />
+      {/* <h2>Blockbuster Deals | Shop Now</h2>
+      <ProductTab /> */}
     </>
   );
 }
-
 
 export default App;
